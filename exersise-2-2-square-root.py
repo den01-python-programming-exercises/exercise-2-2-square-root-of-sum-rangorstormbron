3546ee47111364a169b6aec04e5a094d71d5e0b7
rangorstormbron <77683460+rangorstormbron@users.noreply.github.com>
2021-01-19 15:46:15 +0000

    Add files via upload

---
+++ exersise-2-2-square-root.py
@@ -0,0 +1,12 @@
+import math
+
+def vampire():
+    number = int(input("please in put your first whole number here="))
+    numberb = int(input("please in put your second whole number here="))
+    total = (number)+(numberb)
+    skyforge= math.sqrt(total)
+    print(skyforge)
+
+ 
+vampire()
+
